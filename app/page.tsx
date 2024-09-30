2d0ae804bb6941ff8cfc640428a1fd39cf7c7b40
Dania Jawad <djawad02@gmail.com>
2024-09-30 13:10:06 +0500

    Created a layout of how homepage will be

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <main>All blog details</main>
+      <Footer />
     </main>
   );
 }
